b8d641556863b29a9b9be3e02928fc6b1659f9d1
Rohit-KK15 <105338950+Rohit-KK15@users.noreply.github.com>
2023-12-17 03:26:01 +0530

    added events in StudentPortal

--- src/views/StudentPortal.vue
+++ src/views/StudentPortal.vue
@@ -8,7 +8,6 @@
       <router-link to="/student-attendance" class="route-link"
         >My Attendance</router-link
       >
-      <router-link to="/events" class="route-link">Events</router-link>
       <a
         href="https://www.andhrauniversity.edu.in/img/news/calau.pdf"
         target="_blank"
@@ -29,6 +28,26 @@
         >Logout</router-link
       >
     </div>
+
+    <h2 class="eventsHead">News and Events</h2>
+    <div class="eventScroller">
+      <marquee height="320px" direction="up" scrolldelay="0" scrollamount="3" onmouseover="this.stop();" onmouseout="this.start();" class="marquee-style">
+								<ul>
+								
+								<li><a href="https://services.andhrauniversity.edu.in/au-sports/main.php" target="blank"><span>07/12/2023 :: </span>Andhra University - Inter University South Zone Youth Festival (UNIFEST) 2023-24 </a> </li>
+								
+								
+								<li><a href="https://www.andhrauniversity.edu.in/img/pdf/events/Brouchure- Ecomarine Project Training Program.pdf" target="blank"><span>25/09/2023 :: </span> Training programme on "Microplastics in Marine Environment - analysis, mitigation and management"
+Phase I: 7th to 9th October 2023 &amp;
+Phase II: 10th to 12th November 2023 organised by Department of Marine Living Resources , Andhra University 
+   </a></li>
+
+
+									<li><a href="https://www.andhrauniversity.edu.in/img/nptel.jpg" target="blank"><span>16/08/2023 :: </span>Andhra University has been awarded as "Best New LC" among the top 100 Local Chapters </a>  </li>
+								</ul>
+								</marquee>
+    </div>
+
   </div>
 </template>
 
