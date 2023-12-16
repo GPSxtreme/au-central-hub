dfc62105b531fb8d55d1b4ff0185cb8694dff0f5
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2023-12-17 00:45:48 +0530

    Create AttendanceManagement.vue

---
+++ src/views/AttendanceManagement.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="main">
+    <h1>Attendance Management</h1>
+  </div>
+</template>
+
+<script>
+import "@/styles/attendanceManagement.css";
+export default {
+  name: "AttendanceManagement",
+};
+</script>
+
+<style></style>
