76beb221fdb56caabf4a08929de1ff880461ded2
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2023-12-16 22:57:55 +0530

    minor changes 🔞

--- src/views/StudentResults.vue
+++ src/views/StudentResults.vue
@@ -1,71 +1,53 @@
 <template>
-  <div>
+  <div class="main">
     <h1>Student Results</h1>
     <div class="semesters">
-      <button class="sem-btn" @click="loadResults('Sem-1')">
-        Sem-1
-      </button>
-      <button class="sem-btn" @click="loadResults('Sem-2')">
-        Sem-2
-      </button>
-      <button class="sem-btn" @click="loadResults('Sem-3')">
-        Sem-3
-      </button>
-      <button class="sem-btn" @click="loadResults('Sem-4')">
-        Sem-4
-      </button>
-      <button class="sem-btn" @click="loadResults('Sem-5')">
-        Sem-5
-      </button>
-      <button class="sem-btn" @click="loadResults('Sem-6')">
-        Sem-6
-      </button>
-      <button class="sem-btn" @click="loadResults('Sem-7')">
-        Sem-7
-      </button>
-      <button class="sem-btn" @click="loadResults('Sem-8')">
-        Sem-8
-      </button>
+      <button class="sem-btn" @click="loadResults('Sem-1')">Sem-1</button>
+      <button class="sem-btn" @click="loadResults('Sem-2')">Sem-2</button>
+      <button class="sem-btn" @click="loadResults('Sem-3')">Sem-3</button>
+      <button class="sem-btn" @click="loadResults('Sem-4')">Sem-4</button>
+      <button class="sem-btn" @click="loadResults('Sem-5')">Sem-5</button>
+      <button class="sem-btn" @click="loadResults('Sem-6')">Sem-6</button>
+      <button class="sem-btn" @click="loadResults('Sem-7')">Sem-7</button>
+      <button class="sem-btn" @click="loadResults('Sem-8')">Sem-8</button>
     </div>
 
     <div v-if="selectedSemester">
       <h2>{{ selectedSemester }}</h2>
       <template v-if="results && Object.keys(results).length > 0">
         <!-- Display results in a table -->
-      <table>
-        <thead>
-          <tr>
-            <th>S.No</th>
-            <th>Subject</th>
-            <th>Marks</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(marks, subject, $index) in results" :key="subject">
-            <template v-if="subject !== 'gpa'">
-              <td>{{ $index + 1 }}</td>
-              <td>{{ subject }}</td>
-              <td>{{ marks.toString() }}</td>
-            </template>
-          </tr>
-        </tbody>
-      </table>
+        <table>
+          <thead>
+            <tr>
+              <th>S.No</th>
+              <th>Subject</th>
+              <th>Marks</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(marks, subject, $index) in results" :key="subject">
+              <template v-if="subject !== 'gpa'">
+                <td>{{ $index + 1 }}</td>
+                <td>{{ subject }}</td>
+                <td>{{ marks.toString() }}</td>
+              </template>
+            </tr>
+          </tbody>
+        </table>
 
-      <!-- Display GPA outside the table -->
-      <div>
-        <h2><strong>GPA:</strong> {{ results.gpa }}</h2>
-      </div>
+        <!-- Display GPA outside the table -->
+        <div>
+          <h2><strong>GPA:</strong> {{ results.gpa }}</h2>
+        </div>
       </template>
       <template v-else>
         <h3>No records found for {{ selectedSemester }}.</h3>
       </template>
-      
     </div>
 
     <div class="cgpa">
       <h2><strong>CGPA:</strong> {{ cgpa }}</h2>
     </div>
-
   </div>
 </template>
 
@@ -78,6 +60,7 @@ import {
   getDocs,
 } from "firebase/firestore";
 import { auth } from "@/firebase";
+import "@/styles/studentResults.css";
 export default {
   name: "StudentResults",
   data() {
@@ -90,7 +73,6 @@ export default {
     };
   },
   methods: {
-
     async fetchAndStoreCGPA() {
       const db = getFirestore();
       const userId = auth.currentUser.uid;
@@ -116,19 +98,16 @@ export default {
       if (semesterCollectionExists) {
         const q = query(resultsRef);
         onSnapshot(q, (querySnapshot) => {
-        const data = [];
-        querySnapshot.forEach((doc) => {
-          data.push({...doc.data() });
+          const data = [];
+          querySnapshot.forEach((doc) => {
+            data.push({ ...doc.data() });
+          });
+          this.selectedSemester = semester;
+          this.results = data[0];
         });
-        this.selectedSemester = semester;
-        this.results = data[0];
-      });
+      } else {
+        this.msg = "No records found!!";
       }
-      else{
-        this.msg="No records found!!"
-      }
-
-      
     },
   },
   created() {
