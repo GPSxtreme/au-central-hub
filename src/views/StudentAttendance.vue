c9cfc6637d274684722049c504b51d4d4cbf15ee
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2023-12-16 19:52:39 +0530

    Added student attendance logic

--- src/views/StudentAttendance.vue
+++ src/views/StudentAttendance.vue
@@ -1,11 +1,66 @@
 <template>
   <div>
-    <h1>Student Attendance</h1>
+    <h1>My Attendance</h1>
+    <table v-if="attendanceRecords.length">
+      <thead>
+        <tr>
+          <th>S.No</th>
+          <th>Date</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(record, index) in attendanceRecords" :key="index">
+          <td>{{ index + 1 }}</td>
+          <td>{{ record.date }}</td>
+          <td>{{ record.status }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <p v-else>No attendance records found.</p>
   </div>
 </template>
 
 <script>
+import "@/styles/studentAttendance.css";
+import {
+  getFirestore,
+  collection,
+  query,
+  onSnapshot,
+} from "firebase/firestore";
+import { auth } from "@/firebase";
 export default {
   name: "StudentAttendance",
+  data() {
+    return {
+      attendanceRecords: [],
+    };
+  },
+  created() {
+    const db = getFirestore();
+    const uid = auth.currentUser.uid; // Get the current user's UID
+    const attendanceRef = collection(
+      db,
+      `users/students/profileData/${uid}/attendance`
+    );
+
+    const q = query(attendanceRef);
+
+    onSnapshot(q, (querySnapshot) => {
+      this.attendanceRecords = querySnapshot.docs.map((doc) => {
+        const data = doc.data();
+        let status = data.isWorkingDay
+          ? data.status
+            ? "Present"
+            : "Absent"
+          : "Not Working Day";
+        return {
+          date: data.date,
+          status: status,
+        };
+      });
+    });
+  },
 };
 </script>
