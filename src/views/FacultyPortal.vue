456522f865fc50d59423b142052a34ee33b478eb
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2023-12-17 11:58:11 +0530

    Update FacultyPortal.vue

--- src/views/FacultyPortal.vue
+++ src/views/FacultyPortal.vue
@@ -13,7 +13,7 @@
         class="route-link"
         >College Calendar</a
       >
-      <router-link to="/assignments" class="route-link"
+      <router-link to="/assignment-management" class="route-link"
         >Assignments</router-link
       >
       <router-link to="/attendance-management" class="route-link"
@@ -30,21 +30,47 @@
 
     <h2 class="eventsHead">News and Events</h2>
     <div class="eventScroller">
-      <marquee height="320px" direction="up" scrolldelay="0" scrollamount="3" onmouseover="this.stop();" onmouseout="this.start();" class="marquee-style">
-								<ul>
-								
-								<li><a href="https://services.andhrauniversity.edu.in/au-sports/main.php" target="blank"><span>07/12/2023 :: </span>Andhra University - Inter University South Zone Youth Festival (UNIFEST) 2023-24 </a> </li>
-								
-								
-								<li><a href="https://www.andhrauniversity.edu.in/img/pdf/events/Brouchure- Ecomarine Project Training Program.pdf" target="blank"><span>25/09/2023 :: </span> Training programme on "Microplastics in Marine Environment - analysis, mitigation and management"
-Phase I: 7th to 9th October 2023 &amp;
-Phase II: 10th to 12th November 2023 organised by Department of Marine Living Resources , Andhra University 
-   </a></li>
+      <marquee
+        height="320px"
+        direction="up"
+        scrolldelay="0"
+        scrollamount="3"
+        onmouseover="this.stop();"
+        onmouseout="this.start();"
+        class="marquee-style"
+      >
+        <ul>
+          <li>
+            <a
+              href="https://services.andhrauniversity.edu.in/au-sports/main.php"
+              target="blank"
+              ><span>07/12/2023 :: </span>Andhra University - Inter University
+              South Zone Youth Festival (UNIFEST) 2023-24
+            </a>
+          </li>
 
+          <li>
+            <a
+              href="https://www.andhrauniversity.edu.in/img/pdf/events/Brouchure- Ecomarine Project Training Program.pdf"
+              target="blank"
+              ><span>25/09/2023 :: </span> Training programme on "Microplastics
+              in Marine Environment - analysis, mitigation and management" Phase
+              I: 7th to 9th October 2023 &amp; Phase II: 10th to 12th November
+              2023 organised by Department of Marine Living Resources , Andhra
+              University
+            </a>
+          </li>
 
-									<li><a href="https://www.andhrauniversity.edu.in/img/nptel.jpg" target="blank"><span>16/08/2023 :: </span>Andhra University has been awarded as "Best New LC" among the top 100 Local Chapters </a>  </li>
-								</ul>
-								</marquee>
+          <li>
+            <a
+              href="https://www.andhrauniversity.edu.in/img/nptel.jpg"
+              target="blank"
+              ><span>16/08/2023 :: </span>Andhra University has been awarded as
+              "Best New LC" among the top 100 Local Chapters
+            </a>
+          </li>
+        </ul>
+      </marquee>
     </div>
   </div>
 </template>
