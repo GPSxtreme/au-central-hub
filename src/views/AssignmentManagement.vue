10737a73e48f3ca6ababd75c4f85d00688454ac8
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2023-12-17 11:58:09 +0530

    Create AssignmentManagement.vue

---
+++ src/views/AssignmentManagement.vue
@@ -0,0 +1,221 @@
+<template>
+  <div class="main">
+    <h1>Assignment Management</h1>
+    <hr />
+    <!-- Department Selection -->
+    <div class="content">
+      <div>
+        <label for="department">Select Department:</label>
+        <select
+          id="department"
+          v-model="selectedDepartment"
+          @change="fetchSections"
+        >
+          <option disabled value="">Please select one</option>
+          <option v-for="dept in departments" :key="dept.id" :value="dept.id">
+            {{ dept.deptName }}
+          </option>
+        </select>
+      </div>
+
+      <!-- Section Selection -->
+      <div v-if="sections.length > 0">
+        <label for="section">Select Section:</label>
+        <select id="section" v-model="selectedSection">
+          <option disabled value="">Please select one</option>
+          <option
+            v-for="section in sections"
+            :key="section.id"
+            :value="section.id"
+          >
+            {{ section.name }}
+          </option>
+        </select>
+      </div>
+      <!-- Subject Selection -->
+      <div v-if="subjects.length > 0">
+        <label for="subject">Select Subject:</label>
+        <select id="subject" v-model="selectedSubject">
+          <option disabled value="">Please select one</option>
+          <option
+            v-for="subject in subjects"
+            :key="subject.id"
+            :value="subject.id"
+          >
+            {{ subject.name }}
+          </option>
+        </select>
+      </div>
+    </div>
+
+    <!-- Action Buttons -->
+    <div v-if="selectedSubject" class="btns">
+      <button @click="viewAssignments">View Assignments</button>
+      <button @click="createAssignment">Create Assignment</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getFirestore, collection, onSnapshot } from "firebase/firestore";
+
+export default {
+  name: "AssignmentManagement",
+  data() {
+    return {
+      departments: [],
+      sections: [],
+      subjects: [],
+      selectedDepartment: "",
+      selectedSection: "",
+      selectedSubject: "",
+      selectedSubjectName: "",
+    };
+  },
+  async created() {
+    await this.fetchDepartments();
+  },
+  methods: {
+    async fetchDepartments() {
+      const db = getFirestore();
+      const departmentsRef = collection(db, "departments");
+
+      onSnapshot(
+        departmentsRef,
+        (querySnapshot) => {
+          this.departments = querySnapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }));
+        },
+        (error) => {
+          console.error("Error fetching departments: ", error);
+        }
+      );
+    },
+    fetchSections() {
+      if (!this.selectedDepartment) {
+        this.sections = [];
+        return;
+      }
+
+      const db = getFirestore();
+      const sectionsRef = collection(
+        db,
+        `departments/${this.selectedDepartment}/sections`
+      );
+
+      onSnapshot(
+        sectionsRef,
+        (querySnapshot) => {
+          this.sections = querySnapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }));
+          this.selectedSection = ""; // Reset selected section
+          this.selectedSubject = ""; // Reset subject selection
+          this.subjects = []; // Reset subjects array
+        },
+        (error) => {
+          console.error("Error fetching sections: ", error);
+        }
+      );
+    },
+    fetchSubjects() {
+      if (!this.selectedSection) {
+        this.subjects = [];
+        return;
+      }
+
+      const db = getFirestore();
+      const subjectsRef = collection(
+        db,
+        `departments/${this.selectedDepartment}/sections/${this.selectedSection}/subjects`
+      );
+
+      onSnapshot(
+        subjectsRef,
+        (querySnapshot) => {
+          this.subjects = querySnapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }));
+          this.selectedSubject = ""; // Reset selected subject
+        },
+        (error) => {
+          console.error("Error fetching subjects: ", error);
+        }
+      );
+    },
+    viewAssignments() {
+      const selectedSubject = this.subjects.find(
+        (subject) => subject.id === this.selectedSubject
+      );
+      this.selectedSubjectName = selectedSubject ? selectedSubject.name : "";
+      const map = {
+        selectedDepartmentId: this.selectedDepartment,
+        selectedSectionId: this.selectedSection,
+        selectedSubjectId: this.selectedSubject,
+        selectedSubjectName: this.selectedSubjectName,
+      };
+      this.$router.push({
+        name: "ViewAssignments",
+        params: map,
+      });
+    },
+    createAssignment() {
+      // Pass selectedSubject along with department and section
+    },
+  },
+  watch: {
+    selectedSection(newSection, oldSection) {
+      if (newSection !== oldSection) {
+        this.fetchSubjects();
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.main {
+  min-height: 80vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
+  margin-top: 40px;
+}
+
+label {
+  margin-right: 10px;
+}
+
+select {
+  margin: 10px;
+  padding: 5px;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+}
+
+button {
+  margin: 10px;
+  padding: 10px 15px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #0056b3;
+}
+
+.content {
+  margin-top: 30px;
+}
+.btns {
+  margin-top: 30px;
+}
+</style>
